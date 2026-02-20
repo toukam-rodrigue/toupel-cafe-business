af31c990fd78ac2a826e5ea7ba66ae25e91fbb71
suvorov <suvorov22002@gmail.com>
2026-02-20 20:16:15 +0100

    Initial commit

---
+++ src/main/java/com/pyramid/toupel/ToupelCafeBusinessApplication.java
@@ -0,0 +1,13 @@
+package com.pyramid.toupel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToupelCafeBusinessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToupelCafeBusinessApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pyramid/toupel/ToupelCafeBusinessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pyramid.toupel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToupelCafeBusinessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
